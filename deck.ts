a4ccec0e9df54ff574fc04c76dc3ca967a7a96b2
takker99 <37929109+takker99@users.noreply.github.com>
2023-03-15 17:58:58 +0900

    :sparkles: Add `updated` in Deck and NoteType

--- deck.ts
+++ deck.ts
@@ -1,6 +1,7 @@
 import type { Result } from "./deps/scrapbox.ts";
-import { parse } from "./deps/csv.ts";
+import { BaseLine } from "./deps/scrapbox.ts";
 import type { Deck } from "./deps/deno-anki.ts";
+import { packRows, parseToRows } from "./deps/scrapbox-parser.ts";
 
 /** deckデータの書式が不正だったときに投げるエラー */
 export interface InvalidDeckError {
@@ -8,23 +9,82 @@ export interface InvalidDeckError {
   message: string;
 }
 
-export const parseDeck = (csv: string): Result<Deck, InvalidDeckError> => {
-  const data = parse(csv);
-  const csvData = new Map<string, string>(data.map(([f, s]) => [f, s]));
-  const name = csvData.get("name");
-  if (!name) return { ok: false, value: makeError("Deck name not found.") };
-  const id_ = csvData.get("id");
-  if (!id_) return { ok: false, value: makeError("Deck id not found.") };
-  const id = parseInt(id_);
-  if (isNaN(id)) {
-    return { ok: false, value: makeError("Deck id is not number.") };
+/** ページ本文からDeckを抽出する
+ *
+ * @param lines メタデータつきページ本文
+ * @return 解析結果。テーブルが見つからなければ`undefined`を返す
+ */
+export const parseDeck = (
+  lines: BaseLine[],
+): Result<Deck | undefined, InvalidDeckError> => {
+  if (lines.length === 0) return { ok: true, value: undefined };
+  const packs = packRows(
+    parseToRows(lines.map((line) => line.text).join("\n")),
+    { hasTitle: true },
+  );
+
+  /** deck id */
+  let id: number | undefined;
+  /** deck name */
+  let name: string | undefined;
+  /** deck description */
+  let description: string | undefined;
+  /** the updated time of the deck (UNIX time) */
+  let updated = 0;
+
+  /** 現在読んでいる`pack.rows[0]`の行番号 */
+  let counter = 0;
+  // deckが書かれたtableから各設定を読み出す
+  for (const pack of packs) {
+    switch (pack.type) {
+      case "title":
+      case "line":
+        counter++;
+        break;
+      case "codeBlock":
+        counter += pack.rows.length;
+        break;
+      case "table": {
+        updated = Math.max(
+          ...lines.slice(counter, counter + pack.rows.length).map((line) =>
+            line.updated
+          ),
+          updated,
+        );
+        counter += pack.rows.length;
+        // filenameが"deck"のもののみ通す
+        if (!pack.rows[0].text.endsWith("deck")) break;
+        const table = Object.fromEntries(
+          pack.rows.map(({ text }) => text.trim().split(/\s+/)),
+        ) as Record<string, string>;
+
+        if (Object.hasOwn(table, "name") && !name) {
+          if (!table.name) {
+            return { ok: false, value: makeError("Deck name not found.") };
+          }
+          name = table.name;
+        }
+        if (Object.hasOwn(table, "id") && !id) {
+          if (!table.id) {
+            return { ok: false, value: makeError("Deck id not found.") };
+          }
+          const idNum = parseInt(table.id);
+          if (isNaN(idNum)) {
+            return { ok: false, value: makeError("Deck id is not number.") };
+          }
+          id = idNum;
+        }
+        description ??= table["description"];
+
+        break;
+      }
+    }
   }
 
-  const deck: Deck = { name, id };
-  const description = csvData.get("description");
-  if (description) deck.description = description;
-
-  return { ok: true, value: deck };
+  return {
+    ok: true,
+    value: id && name ? { id, name, description, updated } : undefined,
+  };
 };
 
 const makeError = (message: string): InvalidDeckError => ({
--- noteType.ts
+++ noteType.ts
@@ -1,6 +1,7 @@
 import type { Result } from "./deps/scrapbox.ts";
-import { parse } from "./deps/csv.ts";
+import { BaseLine } from "./deps/scrapbox.ts";
 import type { NoteType } from "./deps/deno-anki.ts";
+import { packRows, parseToRows } from "./deps/scrapbox-parser.ts";
 
 /** note typeデータの書式が不正だったときに投げるエラー */
 export interface InvalidNoteTypeError {
@@ -8,40 +9,21 @@ export interface InvalidNoteTypeError {
   message: string;
 }
 
-export const parseNoteType = (
-  csv: string,
-): Result<NoteType, InvalidNoteTypeError> => {
-  const data = parse(csv);
-  const csvData = new Map<string, string>(data.map(([f, s]) => [f, s]));
-  const name = csvData.get("name");
-  if (!name) {
-    return { ok: false, value: makeError("Note Type name not found.") };
-  }
-  const id_ = csvData.get("id");
-  if (!id_) return { ok: false, value: makeError("Note Type id not found.") };
-  const id = parseInt(id_);
-  if (isNaN(id)) {
-    return { ok: false, value: makeError("Note Type id is not number.") };
-  }
-
-  const answer = csvData.get("answer") ??
-    '{{cloze:Text}}<br><a href="{{SourceURL}}">source</a>';
-  const question = csvData.get("question") ?? "{{cloze:Text}}\n{{type:Text}}";
-  const noteType: NoteType = {
-    name,
-    id,
-    // 今回は穴埋め特化のMVPを作るので、fieldは決め打ちにする
-    fields: [
-      { name: "Text", description: "問題文" },
-      { name: "SourceURL", description: "問題の取得元URL" },
-    ],
-    isCloze: true,
-    templates: [{
-      name: "Card 1",
-      answer,
-      question,
-    }],
-    css: `.card {
+/** 既定のNote type */
+export const defaultNoteType: NoteType = {
+  name: "Basic (Cloze)",
+  id: 1677417085373,
+  fields: [
+    { name: "Text", description: "問題文" },
+    { name: "SourceURL", description: "問題の取得元URL" },
+  ],
+  isCloze: true,
+  templates: [{
+    name: "Card 1",
+    answer: '{{cloze:Text}}<br><a href="{{SourceURL}}">source</a>',
+    question: "{{cloze:Text}}\n{{type:Text}}",
+  }],
+  css: `.card {
   display: flex;
   justify-content: center;
   font-family: arial;
@@ -56,10 +38,114 @@ export const parseNoteType = (
 .nightMode .cloze {
   color: lightblue;
 }`,
-  };
-  // updatedは/api/table/だけからは計算しにくいので省略する
+};
 
-  return { ok: true, value: noteType };
+export const parseNoteType = (
+  lines: BaseLine[],
+): Result<NoteType | undefined, InvalidNoteTypeError> => {
+  if (lines.length === 0) return { ok: true, value: undefined };
+  const packs = packRows(
+    parseToRows(lines.map((line) => line.text).join("\n")),
+    { hasTitle: true },
+  );
+
+  /** note type id */
+  let id: number | undefined;
+  /** note type name */
+  let name: string | undefined;
+  /** the updated time of the note type (UNIX time) */
+  let updated = 0;
+  let answer: string | undefined;
+  let question: string | undefined;
+
+  /** 現在読んでいる`pack.rows[0]`の行番号 */
+  let counter = 0;
+  // deckが書かれたtableから各設定を読み出す
+  for (const pack of packs) {
+    switch (pack.type) {
+      case "title":
+      case "line":
+        counter++;
+        break;
+      case "codeBlock":
+        counter += pack.rows.length;
+        break;
+      case "table": {
+        updated = Math.max(
+          ...lines.slice(counter, counter + pack.rows.length).map((line) =>
+            line.updated
+          ),
+          updated,
+        );
+        counter += pack.rows.length;
+        // filenameが"note type"のもののみ通す
+        if (!pack.rows[0].text.endsWith("note type")) break;
+        const table = Object.fromEntries(
+          pack.rows.map(({ text }) => text.trim().split(/\s+/)),
+        ) as Record<string, string>;
+
+        if (Object.hasOwn(table, "name") && !name) {
+          if (!table.name) {
+            return { ok: false, value: makeError("Note type name not found.") };
+          }
+          name = table.name;
+        }
+        if (Object.hasOwn(table, "id") && !id) {
+          if (!table.id) {
+            return { ok: false, value: makeError("Note type id not found.") };
+          }
+          const idNum = parseInt(table.id);
+          if (isNaN(idNum)) {
+            return {
+              ok: false,
+              value: makeError("Note type id is not number."),
+            };
+          }
+          id = idNum;
+        }
+        if (Object.hasOwn(table, "answer") && !answer) {
+          if (!table.answer) {
+            return {
+              ok: false,
+              value: makeError("answer not found."),
+            };
+          }
+          answer = table.answer;
+        }
+        if (Object.hasOwn(table, "question") && !question) {
+          if (!table.question) {
+            return {
+              ok: false,
+              value: makeError("question not found."),
+            };
+          }
+          question = table.question;
+        }
+
+        break;
+      }
+    }
+  }
+
+  return {
+    ok: true,
+    value: id && name
+      ? {
+        name,
+        id,
+        updated,
+        // 今回は穴埋め特化のMVPを作るので、fieldは決め打ちにする
+        fields: structuredClone(defaultNoteType.fields),
+        isCloze: true,
+        templates: [{
+          name: "Card 1",
+          answer: answer ?? defaultNoteType.templates[0].answer,
+          question: question ?? defaultNoteType.templates[0].question,
+        }],
+        css: defaultNoteType.css,
+      }
+      : undefined,
+  };
 };
 
 const makeError = (message: string): InvalidNoteTypeError => ({
